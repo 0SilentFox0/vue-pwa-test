3cd3c10aef7d60d056f795012c338b30f0849c46
ND <aspizhav@gmail.com>
2024-05-26 21:05:06 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import vue from '@vitejs/plugin-vue';
-import { fork } from 'node:child_process';
-import fs from 'node:fs';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -13,9 +11,9 @@ const envDir = path.resolve(__dirname, './env');
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, envDir, '');
-  if (command === 'serve' && env.NO_PUSH_SERVER?.toUpperCase() !== 'TRUE') {
-    startPushDevelopmentServer();
-  }
+  // if (command === 'serve' && env.NO_PUSH_SERVER?.toUpperCase() !== 'TRUE') {
+  //   startPushDevelopmentServer();
+  // }
 
   return {
     envPrefix: 'PUBLIC_',
@@ -35,21 +33,21 @@ export default defineConfig(({ command, mode }) => {
   };
 });
 
-function startPushDevelopmentServer() {
-  const serverDir = path.resolve(__dirname, 'push-server');
-  const serverNodeModules = path.resolve(serverDir, 'node_modules');
+// function startPushDevelopmentServer() {
+//   const serverDir = path.resolve(__dirname, 'push-server');
+//   const serverNodeModules = path.resolve(serverDir, 'node_modules');
 
-  if (!fs.existsSync(serverNodeModules)) {
-    console.error(
-      '\x1b[31m%s\x1b[90m\x1b[40m%s\x1b[0m\x1b[31m%s\x1b[0m',
-      'Please run ',
-      'cd push-server; npm install',
-      ' first.',
-    );
-    process.exit(1);
-  }
+//   if (!fs.existsSync(serverNodeModules)) {
+//     console.error(
+//       '\x1b[31m%s\x1b[90m\x1b[40m%s\x1b[0m\x1b[31m%s\x1b[0m',
+//       'Please run ',
+//       'cd push-server; npm install',
+//       ' first.',
+//     );
+//     process.exit(1);
+//   }
 
-  const serverFile = path.resolve(serverDir, 'server.js');
+//   const serverFile = path.resolve(serverDir, 'server.js');
 
-  fork(serverFile);
-}
+//   fork(serverFile);
+// }
